36cf34ff3d957ee0f06b5d947f1346ec3ea62ac4
Bernard Kim <bernard@gravitational.com>
2020-02-05 10:33:01 -0800

    Register status history cmd
    - Add `StatusClusterCmd` subcommand and set as default. Kingpin v2 does
    not support optional subcommands. `StatusCmd` now just combines
    `StatusClusterCmd` and `StatusHistoryCmd`.
    - `StatusHistoryCmd` now calls `statusHistory` function.

--- tool/gravity/cli/commands.go
+++ tool/gravity/cli/commands.go
@@ -98,8 +98,10 @@ type Application struct {
 	UpdateSystemCmd UpdateSystemCmd
 	// UpgradeCmd launches app upgrade
 	UpgradeCmd UpgradeCmd
-	// StatusCmd displays cluster status
+	// StatusCmd combines subcommands for displaying status information
 	StatusCmd StatusCmd
+	// StatusClusterCmd displays the current cluster status
+	StatusClusterCmd StatusClusterCmd
 	// StatusHistoryCmd displays the cluster status history
 	StatusHistoryCmd StatusHistoryCmd
 	// StatusResetCmd resets the cluster to active state
@@ -662,8 +664,13 @@ type UpgradeCmd struct {
 	Values *[]string
 }
 
-// StatusCmd displays cluster status
+// StatusCmd combines subcommands for displaying status information
 type StatusCmd struct {
+	*kingpin.CmdClause
+}
+
+// StatusClusterCmd displays current cluster status
+type StatusClusterCmd struct {
 	*kingpin.CmdClause
 	// Token displays only join token
 	Token *bool
